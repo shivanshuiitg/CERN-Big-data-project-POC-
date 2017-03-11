085482b5fdd3165d4c9eb7311b1cbeb233da0f3b
shivanshuiitg <shivanshu5100@gmail.com>
2017-03-11 14:27:05 +0530

    Python functions put in module, real time refreshing implemented and code refactored.

---
+++ functions.py
@@ -0,0 +1,59 @@
+from IPython.core.display import HTML,display
+from string import Template
+import json,requests,threading,time,thread
+import ipywidgets as widgets
+from operator import add
+from multiprocessing import Process
+## Please specify `sparkHost` and `filename`
+## `sparkHost` is the server on which you are running the script
+sparkHost = "localhost"
+filename = 'textfile.txt'
+css_code=open('css/style.css', 'r').read()
+javascript_code=open('js/javascript.js','r').read()
+html_code = open('html/monitor-template.html','r').read()
+
+
+## functions to be used
+
+def get_application_id(sc):
+    ## Returns application-id corresponding to SparkContext
+    return sc._jsc.sc().applicationId()
+
+def get_all_applications(sc):
+    ## Returns JSON String of all applications 
+    url = get_base_url(sc)
+    result = requests.get(url)
+    return json.dumps(result.json())
+
+def get_jobs(app_id,sc):
+    ## Returns JSON String of all jobs associated with app_id
+    url = "{base_url}/{app_id}/jobs".format(base_url=get_base_url(sc),app_id=app_id)
+    result = requests.get(url)
+    return json.dumps(result.json())
+
+def get_base_url(sc):
+    ## Get the port on which UI for this application is running and return base url for querying Spark API
+    for x in range(4040,4060):
+        link = "http://"+sparkHost+":"+str(x)+"/environment/"
+        result = requests.get(link)
+        if ( result.status_code == 200 and sc.applicationId in str(result.content)):
+            return "http://{spark_host}:{ui_port}/api/v1/applications".format(spark_host=sparkHost,ui_port=x)
+def show_html():
+    ## display the Live data as HTML
+    display(HTML(css_code + html_code + javascript_code))
+
+def main(sc,filename):
+	textRDD = sc.textFile(filename)
+   	words = textRDD.flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1))
+   	wordcount = words.reduceByKey(add).collect()
+   	for wc in wordcount:
+		print (wc[0],wc[1])
+   	textRDD.flatMap(lambda line: line.split()).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b)
+   	ls = range(100)
+   	ls_rdd = sc.parallelize(ls, numSlices=3000)
+   	ls_out = ls_rdd.map(lambda x: x+1).collect()
+   	print ('output!: ', ls_out)
+
+def StartApplication(sc,filename):
+	thread.start_new_thread(show_html, ())
+	thread.start_new_thread(main, (sc,filename))
--- js/javascript.js
+++ js/javascript.js
@@ -1,124 +1,163 @@
 <script>
-var iteration = 1
-    function isJSON(str) {
-        try {
-            JSON.parse(str);
-        } catch (e) {
-            return false;
-        }
-        return true;
+// indicator variable for all jobs completion
+var isComplete = 0;
+var iteration = 1;
+// template html string for Job
+var job_html_template_string = '<div class="job-details">\
+<strong style="color:green">Job ID:</strong><span class="job-id"></span><br>\
+<strong style="color:green">Job Name:</strong><span class="job-name"></span><br>\
+<strong style="color:green">Job Status: </strong><span class="job-status"></span>\
+</div>\
+<table border="3" class="job-table">\
+<tbody>\
+<tr style="height: 23px;">\
+<td style="width: 150px; height: 23px;"><strong>Number of Tasks</strong></td>\
+<td style="width: 150px; height: 23px; color: orange;"><strong>Active Tasks</strong></td>\
+<td style="width: 150px; height: 23px; color: green;"><strong>Completed Tasks</strong></td>\
+<td style="width: 150px; height: 23px; color: blue;"><strong>Skipped Tasks</strong></td>\
+<td style="width: 150px; height: 23px; color: red;"><strong>Failed Tasks</strong></td>\
+</tr>\
+<tr style="height: 23px;">\
+<td style="width: 150px; height: 23px;"><span class="number-of-tasks"><span></td>\
+<td style="width: 150px; height: 23px;"><span class="active-tasks"><span></td>\
+<td style="width: 150px; height: 23px;"><span class="completed-tasks"><span></td>\
+<td style="width: 150px; height: 23px;"><span class="skipped-tasks"><span></td>\
+<td style="width: 150px; height: 23px;"><span class="failed-tasks"><span></td>\
+</tr></tbody></table>\
+<span class="job-progress-span">\
+<div class="progress-slider">\
+<div class="progress-slider-bar"></div>\
+<div class="progress-slider-number"></div>\
+</div>\
+</span>\
+<span class="job_submission">\
+<span style="color:green;font-weight:bold;">Job Submission : </span>\
+<span class="job_submission_time"></span>\
+</span>\
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
+<span class="job_completion" style="display:none;">\
+<span style="color:green;font-weight:bold;">Job Completion : </span>\
+<span class="job_completion_time"></span>\
+</span>';
+function isJSON(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
     }
-    function render_html(number_of_jobs){
-        $('#main-div').css("height",(number_of_jobs*150 + 120).toString()+"px");
-        var count = 0;
-        var str = " ";
-        for (count = 0;count <number_of_jobs;count++){
-         str =  str + '<div class="job-div" id="job-'+count+'"><div class="job-details"><strong style="color:green">Job ID:</strong> <strong><span class="job-id">1<span></strong><br><strong style="color:green">Job Name:</strong> <strong><span class="job-name"><span></strong><br><strong style="color:green">Job Status: </strong><strong><span class="job-status"><span><strong></div><table border="3" class="job-table"><tbody><tr style="height: 23px;"><td style="width: 150px; height: 23px;"><strong>Number of Tasks</strong></td><td style="width: 150px; height: 23px; color: orange;"><strong>Active Tasks</strong></td><td style="width: 150px; height: 23px; color: green;"><strong>Completed Tasks</strong></td><td style="width: 150px; height: 23px; color: blue;"><strong>Skipped Tasks</strong></td><td style="width: 150px; height: 23px; color: red;"><strong>Failed Tasks</strong></td></tr><tr style="height: 23px;"><td style="width: 150px; height: 23px;"><span class="number-of-tasks"><span></td><td style="width: 150px; height: 23px;"><span class="active-tasks"><span></td><td style="width: 150px; height: 23px;"><span class="completed-tasks"><span></td><td style="width: 150px; height: 23px;"><span class="skipped-tasks"><span></td><td style="width: 150px; height: 23px;"><span class="failed-tasks"><span></td></tr></tbody></table><span class="job-progress-span"><div class="progress-slider"><div class="progress-slider-bar" id="progress_slider_bar_"'+count+' ></div><div class="progress-slider-number"></div></div></span></div><br>';
-        }
-        $("#job-info").html(str)
-    }
-    function render_live_data(jobs_dict){
-        //alert("Number of Jobs : " + jobs_dict.length)
-        number_of_jobs = jobs_dict.length
-        render_html(number_of_jobs)
-        
-        for (var i = 0;i<number_of_jobs;i++){
-            var job = jobs_dict[i];
-            $("#job-"+i.toString()).find('.number-of-tasks').text(job["numTasks"]);
-            $("#job-"+i.toString()).find('.active-tasks').text(job["numActiveTasks"]);
-            $("#job-"+i.toString()).find('.completed-tasks').text(job["numCompletedTasks"]);
-            $("#job-"+i.toString()).find('.skipped-tasks').text(job["numSkippedTasks"]);
-            $("#job-"+i.toString()).find('.failed-tasks').text(job["numFailedTasks"]);
-            $("#job-"+i.toString()).find('.job-id').text(job["jobId"]);
-            $("#job-"+i.toString()).find('.job-name').text(job["name"]);
-            $("#job-"+i.toString()).find('.job-status').text(job["status"]);
-            var number_of_tasks = job["numTasks"];
-            var processed_tasks = job["numCompletedTasks"] + job["numSkippedTasks"] + job["numFailedTasks"];
-            var percentage_progress = Math.round((processed_tasks*100)/number_of_tasks);
-            console.log(percentage_progress);
-            $("#job-"+i.toString()).find('.progress-slider-number').text(percentage_progress + " %");
-            // TODO
-            // width not changing
-            //$("#progress_slider_bar_"+i.toString()).css("width":percentage_progress+" %");
-            //$("#job-"+i.toString()).find('.progress-slider-number').text(percentage_progress + "%");
-    }
-    }
-    function render_application_data(application_dict){
-            $("#application-name").text(application_dict[0]["name"]);
-            $("#application-id").text(application_dict[0]["id"]);
-            $("#number-of-attempts").text((application_dict[0]["attempts"]).length);
-    }
+    return true;
+}
 
-    function get_output(out){
-        var res = null;
-        if(out.msg_type == "stream"){
-            res = out.content.text;
-        }
-        else if(out.msg_type === "pyout"){
-            res = out.content.data["text/plain"];
-        }
-        else if(out.msg_type == "error"){
-            res = out.content.ename + ": " + out.content.evalue;
-        }
-        else if (out.msg_type == "execute_result"){
-            res = out.content.data["text/plain"];
-        }
-        else{
-            res = "[out type not implemented]";   
-        }
-        return res;
+function render_html(number_of_jobs){
+    $('#main-div').css("height",(number_of_jobs*180 + 70).toString()+"px");
+    var count = 0;
+    var str = " ";
+    for (count = 0;count <number_of_jobs;count++){
+        str =  str + '<div class="job-div" id="job-'+count+'"> ' + job_html_template_string + '</div><br>';
     }
+    $("#job-info").html(str)
+}
+
+function render_live_data(jobs_dict){
+    number_of_jobs = jobs_dict.length
+    render_html(number_of_jobs)
+    var jobs_complete = 0;
+    for (var i = 0;i<number_of_jobs;i++){
+        var job = jobs_dict[i];
+        var number_of_tasks = job["numTasks"];
+        var processed_tasks = job["numCompletedTasks"] + job["numSkippedTasks"] + job["numFailedTasks"];
+        var percentage_progress = Math.round((processed_tasks*100)/number_of_tasks);
+        var width = percentage_progress+"%";
+        var submission_time = job["submissionTime"];
+        var completion_time = " ";
+        $("#job-"+i.toString()).find('.number-of-tasks').text(job["numTasks"]);
+        $("#job-"+i.toString()).find('.active-tasks').text(job["numActiveTasks"]);
+        $("#job-"+i.toString()).find('.completed-tasks').text(job["numCompletedTasks"]);
+        $("#job-"+i.toString()).find('.skipped-tasks').text(job["numSkippedTasks"]);
+        $("#job-"+i.toString()).find('.failed-tasks').text(job["numFailedTasks"]);
+        $("#job-"+i.toString()).find('.job-id').text(job["jobId"]);
+        $("#job-"+i.toString()).find('.job-name').text(job["name"]);
+        $("#job-"+i.toString()).find('.job-status').text(job["status"]);
+        $("#job-"+i.toString()).find('.progress-slider-number').text(percentage_progress + " %");
+        $("#job-"+i.toString()).find('.progress-slider-bar').css("width",width);
+        if(job["status"]== "SUCCEEDED"){
+            jobs_complete = jobs_complete + 1;
+            $("#job-"+i.toString()).find('.stop_job_button').prop('disabled', true);
+        }
+        $("#job-"+i.toString()).find('.job_submission_time').text(submission_time);
+        if ("completionTime" in job){
+            completion_time = job["completionTime"];
+            $("#job-"+i.toString()).find('.job_completion').css("display","");
+            $("#job-"+i.toString()).find('.job_completion_time').text(completion_time);
+        }
+    }
+    if (jobs_complete == number_of_jobs){
+            isComplete = 1;
+            $("#stop_application_button").attr("disabled","disabled");
+    }
+}
+function render_application_data(application_dict){
+    $("#application-name").text(application_dict[0]["name"]);
+    $("#application-id").text(application_dict[0]["id"]);
+    $("#number-of-attempts").text((application_dict[0]["attempts"]).length);
+}
+
+function get_output(out){
+    var res = null;
+    if(out.msg_type == "stream"){
+        res = out.content.text;
+    }
+    else if(out.msg_type === "pyout"){
+        res = out.content.data["text/plain"];
+    }
+    else if(out.msg_type == "error"){
+        res = out.content.ename + ": " + out.content.evalue;
+    }
+    else if (out.msg_type == "execute_result"){
+        res = out.content.data["text/plain"];
+    }
+    else{
+        res = "[out type not implemented]";   
+    }
+    return res;
+}
     
-    function handle_jobs_data_output(out){
-        var result = get_output(out);
-        //console.log(res)
-        var jobs_json_string = result.substr(1).slice(0, -1);
-        //console.log(jobs_json_string)
-        var jobs_dict = JSON.parse(jobs_json_string);
-        //console.log(jobs_dict);
-        //console.log(jobs_dict[0]["status"]);
-        render_live_data(jobs_dict);
-    }
-    function handle_application_data_output(out){
-        var result = get_output(out)
-        var application_json_string = result.substr(1).slice(0, -1);
-        var application_dict = JSON.parse(application_json_string);
-        //alert(application_dict["name"])
-        render_application_data(application_dict)
-    }
-    /**
-    var application_id = " ";
-    function handle_application_id_output(out){
-        var res = null;
-        if(out.msg_type == "stream"){
-            res = out.content.text;
-        }
-        else if(out.msg_type === "pyout"){
-            res = out.content.data["text/plain"];
-        }
-        else if(out.msg_type == "error"){
-            res = out.content.ename + ": " + out.content.evalue;
-        }
-        else if (out.msg_type == "execute_result"){
-            res = out.content.data["text/plain"];
-        }
-        else{
-            res = "[out type not implemented]";   
-        }
-        application_id = res;
-    }
-    **/
-    function exec_code(){
-        var get_application_python_code = "get_all_applications()";
+function handle_jobs_data_output(out){
+    var result = get_output(out);
+    console.log(result)
+    var jobs_json_string = result.substr(1).slice(0, -1);
+    var jobs_dict = JSON.parse(jobs_json_string);
+    console.log(jobs_dict)
+    render_live_data(jobs_dict);
+}
+function handle_application_data_output(out){
+    var result = get_output(out)
+    console.log(result)
+    var application_json_string = result.substr(1).slice(0, -1);
+    var application_dict = JSON.parse(application_json_string);
+    render_application_data(application_dict)
+}
+function exec_code(){
+    if (iteration==1){
+        var get_application_python_code = "fc.get_all_applications(sc)";
         var kernel_1 = IPython.notebook.kernel;
         var msg_id_1 = kernel_1.execute(get_application_python_code,{ 'iopub' : {'output' : handle_application_data_output}}, {silent:false})
-        var get_jobs_python_code = "get_jobs(get_application_id(sc))";
-        var kernel_2 = IPython.notebook.kernel;
-        var callbacks = { 'iopub' : {'output' : handle_jobs_data_output}};
-        var msg_id_2 = kernel_2.execute(get_jobs_python_code, callbacks, {silent:false});
+        iteration = iteration + 1;
     }
-   exec_code()
-  setInterval(function(){
-    exec_code()
-  }, 500);
+    var get_jobs_python_code = "fc.get_jobs(fc.get_application_id(sc),sc)";
+    var kernel_2 = IPython.notebook.kernel;
+    var callbacks = { 'iopub' : {'output' : handle_jobs_data_output}};
+    var msg_id_2 = kernel_2.execute(get_jobs_python_code, callbacks, {silent:false});
+}
+
+exec_code()
+setInterval(function(){
+    // if all jobs are not completed then keep polling
+    if (isComplete==0){
+        exec_code();
+    }
+}, 500);
 </script>
+
+
+
